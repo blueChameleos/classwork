c2e24a5d41728a0f2bb69ce8e28349ba62bdf936
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-10-16 15:33:08 -0400

    lol

--- classwork-repo/src/arrays/ArraysMain.java
+++ classwork-repo/src/arrays/ArraysMain.java
@@ -1,14 +1,89 @@
 package arrays;
 
-public class ArraysMain {
+import java.util.*;
 
+public class ArraysMain {
+	
+	private String[] suits;
+	private String[] values;
+	private String[] deck;
+	
 	public ArraysMain() {
-		// TODO Auto-generated constructor stub
+		suits  = new String[4];
+		
+		suits[0] = "CLUBS";
+		suits[1] = "HEARTS";
+		suits[2] = "DIAMONDS";
+		suits[3] = "SPADES";
+		
+		values = new String[13];
+		
+		for(int i = 0; i < values.length; i++) {
+			values[i] = "" + (i+1);
+		}
+		
+		values[0] = "ACE";
+		values[12] = "KING";
+		values[11] = "QUEEN";
+		values[10] = "JACK";
+		
+		deck = new String[52];
+		buildDeck();
+		
+		System.out.println(Arrays.toString(deck));
+	}
+	
+	private void buildDeck() {
+		
+	}
+	
+	private void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
+	private void populate1ToN(int[] array) {
+		for(int i  = 0; i < array.length; i++) {
+			array[i] = (i + 1);
+		}
+	}
+	
+	public void shuffle(int[] array) {
+		for(int i = 0; i < array.length; i++) {
+			swap(array,i,(int)(Math.random()*array.length));
+		}
+	}
+
+	private void countOccurences(int[] array, int lower, int upper) {
+		int[] counter = new int[upper-lower+1];
+		for(int value: array) {
+			counter[value - lower]++;
+		}
+		for(int i = 0; i < counter.length; i++) {
+			System.out.println("The value " + (i+lower)+ " was rolled " + counter[i] + " times.");
+		}
+	}
+
+	private void populate(int[] array) {
+		for(int i = 0; i < array.length; i++) {
+			array[i] = diceRoll(2);
+		}	
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArraysMain sample = new ArraysMain();
+		
+		//1. Arrays are collections of primitives and Objects
+		//SPECIAL NOTE: This is the ONLY collection of primitives
+		
+		//2. Size cannot be changed
+		
+		//3. Elements of the array are REFERENCES to objects. In
+		//other words, changing an element of an array changes
+		//the reference, NOT the object
+		
+		
 	}
 	
 	/**
@@ -21,11 +96,10 @@ public class ArraysMain {
 		int result = 0;
 		
 		for(int i = 0; i < n; i++) {
-			int randNum = (int)(Math.random()*6) + 1;
-			result += randNum;
+			int dieRoll = (int)(Math.random()*6) + 1;
+			result += dieRoll;
 		}
 		
 		return result;
 	}
-
 }
