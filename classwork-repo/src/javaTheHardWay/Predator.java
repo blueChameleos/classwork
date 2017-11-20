9ee672c716d4bd493fa719c53aafd08eb029a55b
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-11-20 14:21:36 -0500

    added Wilderness, Predator, and Prey

---
+++ classwork-repo/src/javaTheHardWay/Predator.java
@@ -0,0 +1,14 @@
+package javaTheHardWay;
+
+public class Predator extends Animal {
+
+	public Predator(String description, Trait trait1, Trait trait2) {
+		super(description, trait1, trait2);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Predator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ classwork-repo/src/javaTheHardWay/Prey.java
@@ -0,0 +1,14 @@
+package javaTheHardWay;
+
+public class Prey extends Animal {
+
+	public Prey(String description, Trait trait1, Trait trait2) {
+		super(description, trait1, trait2);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Prey() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
